65a1e23e20c6155e19a63fdf3752335c6ad8d05a
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-05-15 17:56:01 +0300

    hooks-app-refact

---
+++ src/components/App/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import Section from '../Section/Section';
+import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
+import Statistics from '../Statistics/Statistics';
+import Notification from '../Notification/Notification';
+import StyledApp from './App.styled';
+
+function FeedbackAll() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  const handleLeaveFeedback = option => {
+    if (option === 'good') {
+      setGood(good => good + 1);
+    }
+    if (option === 'neutral') {
+      setNeutral(neutral => neutral + 1);
+    }
+    if (option === 'bad') {
+      setBad(bad => bad + 1);
+    }
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <StyledApp>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        )}
+      </Section>
+    </StyledApp>
+  );
+}
+
+export default FeedbackAll;
